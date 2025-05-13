cfd259219fb4a7e29499bba15e1849495ec2db9e
lg14uni-leipzig <167429251+lg14uni-leipzig@users.noreply.github.com>
2025-05-13 19:35:19 +0200

    Delete Visualisierung directory

--- Visualisierung/data.py
+++
@@ -1,4 +0,0 @@
-from pandas import DataFrame, read_csv
-
-def load(path: str) -> DataFrame:
-	return read_csv(path, quotechar="'").drop_duplicates()
--- Visualisierung/id.py
+++
@@ -1,4 +0,0 @@
-from uuid import uuid4
-
-def generate_id(prefix: str):
-	return f"{prefix}-{uuid4()}"
