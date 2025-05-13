53f994cbb6bebcbbaa714ce878514ea696b2f308
lg14uni-leipzig <167429251+lg14uni-leipzig@users.noreply.github.com>
2025-05-13 23:19:00 +0200

    Add files via upload

--- Visualisierung/visualizations/distribution.py
+++ Visualisierung/visualizations/distribution.py
@@ -6,7 +6,7 @@ from dash.dependencies import Output
 from dash.development.base_component import Component
 from dash.html import H3
 from pandas import DataFrame
-from plotly.express import histogram
+from plotly.express import histogram, scatter
 from filter import and_, eq, number_range, multi_eq
 from id import generate_id
 from input import build_dropdown, build_radio_items, build_rangeslider
@@ -18,7 +18,10 @@ class Distribution(Visualization):
 		self.title = H3("Verteilung der Items")
 
 		GRAPH_ID = generate_id("graph-distribution")
+		SCATTER_ID = generate_id("graph-scatter") # Neue ID für Scatterplot
+
 		self.graph = Graph(GRAPH_ID)
+		self.scatter_graph = Graph(SCATTER_ID) # Neuer Graph für Scatterplot
 		self.show_boxplot = show_boxplot
 
 		self.filter_elements =  [
@@ -28,6 +31,7 @@ class Distribution(Visualization):
 			build_rangeslider("nach Age filtern", int(dataframe["Age"].min()), int(dataframe["Age"].max())),
 			build_rangeslider("nach Wage (in Euro) filtern", int(dataframe["Wage(in Euro)"].min()), int(dataframe["Wage(in Euro)"].max())),
 			build_rangeslider("nach Overall filtern", int(dataframe["Overall"].min()), int(dataframe["Overall"].max()))
+			build_radio_items("Scatter Y-Achse", ["Wage (in Euro)", "Overall"], "Wage (in Euro)")
 		]
 
 
@@ -37,6 +41,7 @@ class Distribution(Visualization):
 				tuple(filter_element["input"] for filter_element in self.filter_elements),
 			]
 		)
+
 		def update(
 			filters: tuple[str, Union[list[str], str], Union[list[str],str], list[int], list[int], list[int]]
 		):
@@ -54,10 +59,43 @@ class Distribution(Visualization):
 				marginal="box" if self.show_boxplot else None,
 				hover_data=["Full Name", "index"]
 			)
-
+								
 			return chart
+		
+
+		@app.callback(
+            Output(SCATTER_ID, "figure"),
+            inputs=[
+                [f["input"] for f in self.filter_elements]
+
+            ]
+        )
+
+        def update_scatter(
+            filters: tuple[str, list[int], list[int], list[int]]
+			#scatter_y: str, age_range: list[int], wage_range: list[int], overall_range: list[int]
+        ):
+            scatter_y, age_range, wage_range, overall_range = filters
+		
+			scatter_data = scatter(
+				and_(dataframe, [
+					number_range("Age", age_range[0], age_range[1]),
+					number_range("Wage(in Euro)", wage_range[0], wage_range[1]),
+					number_range("Overall", overall_range[0], overall_range[1])
+				]).reset_index(),
+				x="Age",
+				y=scatter_y,
+				opacity=0.4, # Transparenz einzelner Datenpunkte
+				color="blue",
+				marginal="box" if self.show_boxplot else None,
+				hover_data=["Full Name", "index"]
+			
+			)
+
+            return scatter_data
+
 
 	def html(self) -> Sequence[Component]:
-		return [self.title, self.graph] + [
+		return [self.title, self.graph, self.scatter_graph] + [
 			input_element["element"] for input_element in self.filter_elements
 		]
